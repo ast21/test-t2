facdc1d08a8bb86438b449ec9fd3ab64be7d432f
Anastas Mironov <ast@l80.ru>
2024-09-25 22:29:00 +0900

    feat: add all command infos

--- app/Console/Commands/FarmLife.php
+++ app/Console/Commands/FarmLife.php
@@ -18,6 +18,8 @@ class FarmLife extends Command
         $this->info('Добро пожаловать в симулятор деревенской жизни!');
 
         // Система должна добавить животных в хлев (10 коров и 20 кур).
+        $this->newLine();
+        $this->info('Создаем ферму и добавляем животных: 10 коров и 20 кур.');
         for($i = 0; $i < 10; $i++) {
             $farm->addAnimal(new Cow());
         }
@@ -33,6 +35,8 @@ class FarmLife extends Command
         }
 
         // 7 раз (неделю) произвести сбор продукции (подоить коров и собрать яйца у кур).
+        $this->newLine();
+        $this->info('Начинаем сбор продукции: прошло 7 дней...');
         for($i = 0; $i < 7; $i++) {
             $farm->harvest();
         }
@@ -43,8 +47,34 @@ class FarmLife extends Command
         foreach ($farm->getCollectedProducts() as $product) {
             $this->info("$product->name: $product->count $product->unitMeasure");
         }
+
         // Добавить на ферму ещё 5 кур и 1 корову (съездили на рынок, купили животных).
+        $this->newLine();
+        $this->info('На ферму привезли новых животных: 5 кур и 1 корову.');
+        for($i = 0; $i < 5; $i++) {
+            $farm->addAnimal(new Chicken());
+        }
+        $farm->addAnimal(new Cow());
+
         // Снова вывести информацию о количестве каждого типа животных на ферме.
-        // Снова 7 раз (неделю) производим сбор продукции и выводим результат на экран.
+        $this->newLine();
+        $this->info("На ферме живут:");
+        foreach ($farm->getAllAnimals() as $animal) {
+            $this->info("$animal->name: $animal->count");
+        }
+
+        // Снова 7 раз (неделю) производим сбор продукции.
+        $this->newLine();
+        $this->info('Начинаем сбор продукции: прошло 7 дней...');
+        for($i = 0; $i < 7; $i++) {
+            $farm->harvest();
+        }
+
+        // И выводим результат на экран.
+        $this->newLine();
+        $this->info('Собрано продукции:');
+        foreach ($farm->getCollectedProducts() as $product) {
+            $this->info("$product->name: $product->count $product->unitMeasure");
+        }
     }
 }
