4c36bc96215ed6e3cb32df532500a4c1509477d0
Anastas Mironov <ast@l80.ru>
2024-09-25 22:46:23 +0900

    refactor: remove duplicated codes

--- app/Console/Commands/FarmLife.php
+++ app/Console/Commands/FarmLife.php
@@ -19,58 +19,62 @@ class FarmLife extends Command
 
         // Система должна добавить животных в хлев (10 коров и 20 кур).
         $this->newLine();
-        $this->info('Создаем ферму и добавляем животных: 10 коров и 20 кур.');
-        for($i = 0; $i < 10; $i++) {
-            $farm->addAnimal(new Cow());
-        }
-        for($i = 0; $i < 20; $i++) {
-            $farm->addAnimal(new Chicken());
-        }
+        $this->comment('Создаем ферму и добавляем животных: 10 коров и 20 кур.');
+        $this->addAnimals($farm, Cow::class, 10);
+        $this->addAnimals($farm, Chicken::class, 20);
 
         // Вывести на экран информацию о количестве каждого типа животных на ферме.
-        $this->newLine();
-        $this->info("На ферме живут:");
-        foreach ($farm->getAllAnimals() as $animal) {
-            $this->info("$animal->name: $animal->count");
-        }
+        $this->printAnimals($farm);
 
         // 7 раз (неделю) произвести сбор продукции (подоить коров и собрать яйца у кур).
-        $this->newLine();
-        $this->info('Начинаем сбор продукции: прошло 7 дней...');
-        for($i = 0; $i < 7; $i++) {
-            $farm->harvest();
-        }
+        $this->harvest($farm);
 
         // Вывести на экран общее кол-во собранной за неделю продукции каждого типа.
-        $this->newLine();
-        $this->info('Собрано продукции:');
-        foreach ($farm->getCollectedProducts() as $product) {
-            $this->info("$product->name: $product->count $product->unitMeasure");
-        }
+        $this->printCollectedProducts($farm);
 
         // Добавить на ферму ещё 5 кур и 1 корову (съездили на рынок, купили животных).
         $this->newLine();
-        $this->info('На ферму привезли новых животных: 5 кур и 1 корову.');
-        for($i = 0; $i < 5; $i++) {
-            $farm->addAnimal(new Chicken());
-        }
-        $farm->addAnimal(new Cow());
+        $this->comment('На ферму привезли новых животных: 5 кур и 1 корову.');
+        $this->addAnimals($farm, Cow::class, 1);
+        $this->addAnimals($farm, Chicken::class, 5);
 
         // Снова вывести информацию о количестве каждого типа животных на ферме.
+        $this->printAnimals($farm);
+
+        // Снова 7 раз (неделю) производим сбор продукции.
+        $this->harvest($farm);
+
+        // И выводим результат на экран.
+        $this->printCollectedProducts($farm);
+    }
+
+    private function harvest(Farm $farm): void
+    {
+        $this->newLine();
+        $this->comment('Начинаем сбор продукции: прошло 7 дней...');
+        for($i = 0; $i < 7; $i++) {
+            $farm->harvest();
+        }
+    }
+
+    private function printAnimals(Farm $farm): void
+    {
         $this->newLine();
         $this->info("На ферме живут:");
         foreach ($farm->getAllAnimals() as $animal) {
             $this->info("$animal->name: $animal->count");
         }
+    }
 
-        // Снова 7 раз (неделю) производим сбор продукции.
-        $this->newLine();
-        $this->info('Начинаем сбор продукции: прошло 7 дней...');
-        for($i = 0; $i < 7; $i++) {
-            $farm->harvest();
+    private function addAnimals(Farm $farm, string $animal, int $count): void
+    {
+        for($i = 0; $i < $count; $i++) {
+            $farm->addAnimal(new $animal());
         }
+    }
 
-        // И выводим результат на экран.
+    private function printCollectedProducts(Farm $farm): void
+    {
         $this->newLine();
         $this->info('Собрано продукции:');
         foreach ($farm->getCollectedProducts() as $product) {
